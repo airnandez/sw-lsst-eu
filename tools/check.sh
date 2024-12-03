d64179564dfa6a52780e9359bb901b36f88f160d
Fabio Hernandez <fairnandez@gmail.com>
2024-12-03 13:32:13 +0100

    Make messages explicit

--- tools/check.sh
+++ tools/check.sh
@@ -20,8 +20,8 @@ fi
 
 source /etc/cvmfs/config.d/sw.lsst.eu.conf
 
-urls=`echo ${CVMFS_SERVER_URL} | sed -e 's/;/ /g' -e 's|@fqrn@|sw.lsst.eu/.cvmfspublished|g'`
-proxies=`echo ${CVMFS_HTTP_PROXY} | sed -e 's/|/ /g' -e 's/;/ /g'`
+urls=$(echo ${CVMFS_SERVER_URL} | sed -e 's/;/ /g' -e 's|@fqrn@|sw.lsst.eu/.cvmfspublished|g')
+proxies=$(echo ${CVMFS_HTTP_PROXY} | sed -e 's/|/ /g' -e 's/;/ /g')
 
 rc=0
 for u in ${urls}; do
@@ -31,10 +31,12 @@ for u in ${urls}; do
       else
          proxy="--proxy ${p}"
       fi
-      ok=`${curlCmd} --silent --head ${proxy} ${u} | grep "HTTP/1.1 200 OK"`
+      ok=$(${curlCmd} --silent --head ${proxy} ${u} | grep "HTTP/1.1 200 OK")
       if [[ -z ${ok} ]]; then
-         echo "HTTP HEAD request for $u via proxy $p failed"
+         echo "⛔️ HTTP HEAD ${u} via (proxy ${p}) failed"
          rc=1
+      else
+         echo "✅ HTTP HEAD ${u} via (proxy ${p}) succeeded"
       fi
    done
 done
